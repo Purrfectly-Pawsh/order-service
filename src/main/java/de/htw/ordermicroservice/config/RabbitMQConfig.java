a76fd89323e8a1a97f28ed9e40335de0e53eb79f
Kristiyan <74624512+krisg9@users.noreply.github.com>
2024-07-11 15:31:05 +0200

    PP-106 - fix queues (#2)

--- src/main/java/de/htw/ordermicroservice/config/RabbitMQConfig.java
+++ src/main/java/de/htw/ordermicroservice/config/RabbitMQConfig.java
@@ -1,9 +1,6 @@
 package de.htw.ordermicroservice.config;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -22,8 +19,8 @@ public class RabbitMQConfig {
     }
 
     @Bean
-    public TopicExchange orderTopicExchange() {
-        return new TopicExchange(ORDER_EXCHANGE);
+    public FanoutExchange orderTopicExchange() {
+        return new FanoutExchange(ORDER_EXCHANGE);
     }
 
     @Bean
@@ -33,7 +30,7 @@ public class RabbitMQConfig {
 
     @Bean
     public Binding bindCreateOrderQueue() {
-        return BindingBuilder.bind(createOrderQueue()).to(orderTopicExchange()).with("order.create");
+        return BindingBuilder.bind(createOrderQueue()).to(orderTopicExchange());
     }
 
     @Bean
